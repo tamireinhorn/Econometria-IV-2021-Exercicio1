db381b3a9958b9a4e55d57409e4bc7cedd21cdfd
Tamir Einhorn Salem <tamir.salem@elevaeducacao.com.br>
2021-04-22 17:56:46 -0300

    WIP Ridge Works

--- Misc.R
+++ Misc.R
@@ -27,14 +27,16 @@ RollingWindow <- function(key_stock = 'SPY', window_size = 1000, week = 5, month
   ##So I always start in 
   ##Say my window is of size 3. Then I would get observations 22, 23, 24.
   ##The end of my interval will be at window_Size -1  + i + month -1 => 2 + 22 = 24
-  i <- 1
+  i <- 300
+  ridge_df <- data.frame(matrix(NA, nrow = days-window_size+1, ncol = 32))#THIS SHOULD BE NUMBER OF WINDOWS TODO
+  lambda_list <- vector(mode = 'list', length = days-window_size+1)
   
-  for(i in 1:days){ ##This cannot go on until days, this needs to stop at the last possible window. ##days - window_size +1 maybe
+  for(i in 1:(days-window_size+1)){ ##This cannot go on until days, this needs to stop at the last possible window. ##days - window_size +1 maybe
     
     beg <- i + (month - 1) ##Just to clear notation, I wanted to create this variable for the interval's beginning.
-    key_window <- key_stock_df[(beg):(window_size - 1 + beg),]  ##This creates the data subset we wish to use ##TODO: should this use the COMPLETE df or the separate?
-    other_window <- wide_other_stock_df [(beg):(window_size - 1 +beg ),]
+    key_window <- key_stock_df[(beg+1):(window_size + beg),]  ##This creates the data subset we wish to use ##TODO: should this use the COMPLETE df or the separate?
     
+   
     ##However, this does not seal the deal. Once inside this window, we need to construct the variables. This requires us to loop again?
     ##If our window_size is indeed 3, and we have observations 22, 23, 24, we need to build RV_m for all these
     ##RV_{m, 22} is 1/22 * sum(RV of 22 to 1), RV_{m,23} is 1/22 * sum(RV of 23 to 2), etc. 
@@ -44,39 +46,40 @@ RollingWindow <- function(key_stock = 'SPY', window_size = 1000, week = 5, month
     RV_w <- vector(mode = 'list', length = window_size)
     RV_t <- vector(mode = 'list', length = window_size)
     current_date <- vector(mode = 'list', length = window_size)
-    past_date <- vector(mode = 'list', length = window_size)
+    future_date <- vector(mode = 'list', length = window_size)
+    
     date_list <- key_stock_df$Date
       for(k in 0:(window_size-1)){
         RV_m[[(k+1)]] <- (1/month) * sum(( key_stock_df$RV[(beg+k):(i+k)]))
         RV_w[[(k+1)]]<- (1/week) * sum(( key_stock_df$RV[(beg+k):(beg+k - (week-1))]))
-        RV_t[[(k+1)]] <- key_stock_df$RV[(beg+k-1)]
+        RV_t[[(k+1)]] <- key_stock_df$RV[(beg+k)]
         current_date[[(k+1)]] <- key_stock_df$Date[(beg+k)]
-        past_date[[(k+1)]] <- key_stock_df$Date[(beg+k-1)]
+        future_date[[(k+1)]] <- key_stock_df$Date[(beg+k+1)]
       }
     ##Now that we've built all of our variables as lists, we wish to assemble them into a dataframe,
     ##We do so by using I(list) to consider it as a column, and then unlist so that the datatype is not a list itself
     independent_variables <-  
-      data.frame('month_RV' = unlist(I(RV_m)), 'week_RV' = unlist(I(RV_w)), 'previous_RV' = unlist(I(RV_t)), 'current_date' = unlist(I(current_date)), 'past_date' = unlist(I(past_date)))
+      data.frame('month_RV' = unlist(I(RV_m)), 'week_RV' = unlist(I(RV_w)), 'previous_RV' = unlist(I(RV_t)), 'current_date' = unlist(I(current_date)), 'future_date' = unlist(I(past_date)))
     
     
     independent_variables$current_date <- as.Date(independent_variables$current_date, origin = '1970-01-01') ##R likes to convert dates to int (nb of days since 1970-01-01), this converts it back
     
     
-    independent_variables$past_date <- as.Date(independent_variables$past_date, origin = '1970-01-01')                
-    independent_variables <- inner_join(independent_variables, wide_other_stock_df, by = c('past_date' = 'Date' )) ## Now, we want to join this with the RV_{t-1} of all other stocks
+    independent_variables$future_date <- as.Date(independent_variables$future_date, origin = '1970-01-01')                
+    independent_variables <- inner_join(independent_variables, wide_other_stock_df, by = c('current_date' = 'Date' )) ## Now, we want to join this with the RV_{t} of all other stocks
     ##Finally, I want to join this with the dependent variable which we separated in key_window.
-    full_data <- key_window %>% select(Date, VOL, chosen_variable) %>% inner_join(independent_variables, by = c('Date' = 'current_date'))
+    full_data <- key_window %>% select(Date, VOL, chosen_variable) %>% inner_join(independent_variables, by = c('Date' = 'future_date'))
     
-    
+   
     y_var <- data.matrix(scale(full_data$RV)) ##GLMNET only works with data matrix, so we need to convert it and separate the variables. Ridge Regression also imposes that the response is centered.
-    x_var <- data.matrix(scale(full_data[,!names(full_data) %in% c('Date', 'VOL', 'RV', 'past_date')])) ##Here, I HAD to remove V, because it's NAN before 2008.
-    
+    x_var <- data.matrix(scale(full_data[,!names(full_data) %in% c('Date', 'VOL', 'RV', 'current_date')])) ##Here, I HAD to remove V, because it's NAN before 2008.
+    names(ridge_df) <- names(x_var)
     ##If I remove it as planned from my dataset, it won't be a problem anymore.
     # Setting the range of lambda values
     
     # Using glmnet function to build the ridge regression in r
    
-    lambda_sequence <- 10^seq(4, -4, by = -.01) ##Create a lambda sequence to test
+    lambda_sequence <- 10^seq(4, -4, by = -.1) ##Create a lambda sequence to test
   
     aic <- c()
     bic <- c()
@@ -94,11 +97,13 @@ RollingWindow <- function(key_stock = 'SPY', window_size = 1000, week = 5, month
     }
     
     lambda_bic <- lambda_sequence[which.min(bic)] #Chooses the best lambda for BIC criteria.
+    lambda_list[[i]] <- lambda_bic
     ridge <-  tidy(glmnet(x_var, y_var, alpha = 0, lambda = lambda_bic)) ##Creates DF for Ridge with optimal Lambda
+    ridge_df[i:i,] = ridge$estimate
+    names(ridge_df) <- ridge$term
+
     
-    
-   
- return(i) }
+ }
   
   
   
