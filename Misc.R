9e3d9d27967b9e513f5ff0c33ead7032a74947c2
Tamir <tamtamireinhorn@gmail.com>
2021-04-24 01:47:13 -0300

    Criando mais funções auxiliares

--- Misc.R
+++ Misc.R
@@ -1,20 +1,20 @@
 ###This should be the R script where we save our functions, because this makes it all much tidier.
 
-VariableCreation <- function(key_stock = 'SPY', window_size = 1000, week = 5, month = 22, df, model_type = 'level'){
+VariableCreation <- function(key_stock = 'SPY', week = 5, month = 22, df){
   ##The key_stock argument defines the stock over which we want to model. It defaults to SPY since it is the ETF.
   
   ##The window_size is the size of our rolling window, which is defaulted to be 1000 as defined by the assignment.
   
   ##The week and month arguments are just for specifying the RV_m, RV_w part of the calculation, should we want to change them as well.
   
-  ##model_type will give us if the model is to be a log or level model and therefore which variable to choose later:
-  chosen_variable <- ifelse(model_type == 'level', 'RV', 'log_RV')
+
+  
   
   other_stock_df <- df %>% filter(Stock != key_stock) ##This will create a dataframe with all other stocks other than our dependent variable!
   
   
   
-  wide_other_stock_df <- other_stock_df %>% select(Stock, Date, RV) %>% dcast(Date ~ Stock, value.var = chosen_variable)  ##This other_stock df needs to be wide!
+  wide_other_stock_df <- other_stock_df %>% select(Stock, Date, RV) %>% dcast(Date ~ Stock, value.var = 'RV')  ##This other_stock df needs to be wide!
   
   
   key_stock_df <- df %>% filter(Stock == key_stock) ##This, in turn, creates a df with ONLY the stock we want.
@@ -22,32 +22,6 @@ VariableCreation <- function(key_stock = 'SPY', window_size = 1000, week = 5, mo
   days <- dim(key_stock_df)[[1]] ##This will give us the length of our dataset
   day_list <- sort(unique(key_stock_df$Date))
   length_rw <- days - (month - 1) - 1 
-}
-
-
-RollingWindow <- function(key_stock = 'SPY', window_size = 1000, week = 5, month = 22, df, model_type = 'level'){
-  ##The key_stock argument defines the stock over which we want to model. It defaults to SPY since it is the ETF.
-  
-  ##The window_size is the size of our rolling window, which is defaulted to be 1000 as defined by the assignment.
-  
-  ##The week and month arguments are just for specifying the RV_m, RV_w part of the calculation, should we want to change them as well.
-  
-  ##model_type will give us if the model is to be a log or level model and therefore which variable to choose later:
-  chosen_variable <- ifelse(model_type == 'level', 'RV', 'log_RV')
-  
-  other_stock_df <- df %>% filter(Stock != key_stock) ##This will create a dataframe with all other stocks other than our dependent variable!
-
- 
-  
-  wide_other_stock_df <- other_stock_df %>% select(Stock, Date, RV) %>% dcast(Date ~ Stock, value.var = chosen_variable)  ##This other_stock df needs to be wide!
-  
-  
-  key_stock_df <- df %>% filter(Stock == key_stock) ##This, in turn, creates a df with ONLY the stock we want.
-  
-  days <- dim(key_stock_df)[[1]] ##This will give us the length of our dataset
-  day_list <- sort(unique(key_stock_df$Date))
-  length_rw <- days - (month - 1) - 1 
-  #length_df would be the number of dates for which RV_m, RV_w and RV_t will be constructed, to be used in all windows eventually estimated. We eliminate the first 21 dates, and the last one, because the last one 
   #would contain regressors, but there would be no future date for these regressors to forecast (that is, there would be no dependent variable), so we have to stop in the observation right before the last one (as it will allow for regressors and a dependent variable, which will be the last observation)
   
   #We construct regressors below. The dependent variable will simply be the RV column in key_stock_df, and it will later be combined correctly with the regressors through an inner_join, taking into account the fact that regressors are one period before the dependent variable they forecast 
@@ -60,11 +34,11 @@ RollingWindow <- function(key_stock = 'SPY', window_size = 1000, week = 5, month
   
   for(i in 1:length_rw){
     
-    RV_m[[i]] <- (1/month) * sum(key_stock_df$RV[i:(i+21)]) #for i=1 (index of the first OBS in the original SPY data for which RV_t RV_m and RV_w are constructed), we sum over observations 1 to 22 of ORIGINAL spy data, yielding the first observation for our regressor
-    RV_w[[i]] <- (1/week) * sum(key_stock_df$RV[(i+17):(i+21)]) #for i=1, we sum over observations 18 to 22
-    RV_t[[i]] <- key_stock_df$RV[(i+21)] #for i=1, we select observation 22
-    current_date[[i]] <- key_stock_df$Date[(i+21)]
-    future_date[[i]] <- key_stock_df$Date[(i+21+1)] #if the code works, the last observation of future_date will be the last date for which SPY has an observation. Testei e ta ok
+    RV_m[[i]] <- (1/month) * sum(key_stock_df$RV[i:(i+month-1)]) #for i=1 (index of the first OBS in the original SPY data for which RV_t RV_m and RV_w are constructed), we sum over observations 1 to 22 of ORIGINAL spy data, yielding the first observation for our regressor
+    RV_w[[i]] <- (1/week) * sum(key_stock_df$RV[(i+17):(i+month-1)]) #for i=1, we sum over observations 18 to 22
+    RV_t[[i]] <- key_stock_df$RV[(i+month-1)] #for i=1, we select observation 22
+    current_date[[i]] <- key_stock_df$Date[(i+month-1)]
+    future_date[[i]] <- key_stock_df$Date[(i+month-1+1)] #if the code works, the last observation of future_date will be the last date for which SPY has an observation. Testei e ta ok
   }
   #Below we form our dataframe of independent variables, using regressors as regular numeric and date coluns
   
@@ -81,8 +55,60 @@ RollingWindow <- function(key_stock = 'SPY', window_size = 1000, week = 5, month
   #Now, we combine the dependent variable with the independent variables above. For a certain date, each dependent variable is paired with the independent variable of the previous date, as it will forecast the dependent
   
   full_data <- key_stock_df %>% select(Date, RV) %>% inner_join(independent_variables, by = c('Date' = 'future_date')) 
+return(full_data)}
+
+RidgeRegressionBIC <- function(x,y){
+  ridge <- glmnet(x, y, alpha = 0) ##This makes a ridge model with the function variables. 
   
-  #renomear RV em SPY por dependent
+  #initializing the BIC vector
+  BIC = rep(NA, length(ridge$lambda))
+  ##The BIC criteria needs to calculate df(lambda), which is given by trace (or sum of the elements of diag)
+  ##The matrix is given by X * Inverse((X'X + lambda * Identity)) * X'
+  x_squared <- t(x) %*% x #transpose of x multiplied (in the matrix sense) by x
+  
+  identity_matrix <- diag(ncol(x)) ##This creates the identity matrix I.
+  aux <-(log(window_size)/window_size)
+  
+  #calculating information criteria
+  for (j in 1:length(ridge$lambda)) { 
+    
+    #degrees of freedom
+    lambda_diag = ridge$lambda[j] * identity_matrix ##This is Lambda * Identity
+    
+    df = sum(diag(x %*% solve(x_squared + lambda_diag) %*% t(x))) + 1 #+1 due to the the intercept! 
+    ##This completes the degrees of freedom calculation!
+    #In slide 6 of lecture 2, part 3, we have that df = tr(x(x'*x+lambda*i)^(-1)*x')
+    #the term (x'*x+lambda*i)^(-1) is obtained by inverting (x'*x+lambda*i), hence we use solve()
+   
+    
+    #sigma^2(lambda)
+    
+    #for yhat, we take into account also the intercept, and for this, we add to X a column of constant 1, which when multiplied by coef(ridge[intercept]), will yield the correct value
+    yhat = cbind(1, x_var[i:(window_size+i-1),]) %*% coef(ridge)[,j]
+    
+    #resid is a vector composed of 1000 elements, each one the residual for each observation in the window
+    resid = (y- yhat)
+    
+    #for sig2, we sum the square of each element of resid. Thus, do resid^d and sum its elements
+    sig2 = sum(resid^2) / (window_size - df)
+    
+    BIC[j] = log(sig2) + df * aux
+    
+  } #end of information criteria
+  ##After completing the calculation of the BIC criteria for every possible lambda in glmnet, all we have to do is get the optimal:
+  lambda_bic = ridge$lambda[which.min(BIC)]
+  final_ridge <- glmnet(x,y, lambda = lambda_bic, alpha = 0)
+  
+return(final_ridge)}
+
+RollingWindow <- function(df, window_size = 1000){
+  ##The key_stock argument defines the stock over which we want to model. It defaults to SPY since it is the ETF.
+  
+  ##The window_size is the size of our rolling window, which is defaulted to be 1000 as defined by the assignment.
+  
+  ##The week and month arguments are just for specifying the RV_m, RV_w part of the calculation, should we want to change them as well.
+  
+ 
   
   #A given date in dep (dependent variable) needs to match the "future_date" of the independent variables
   
@@ -117,7 +143,7 @@ RollingWindow <- function(key_stock = 'SPY', window_size = 1000, week = 5, month
   lambda_ridge <- rep(NA, num_windows)
   forecast_ridge <- rep(NA, num_windows)
   betas_ridge <- matrix(nrow = dim(x_var)[2], ncol = num_windows ) #we ignore the intercept, as we are interest in the model`s regressors (there are 31 = dim(x_var)[2]))) 
-  i <- 2
+  i <- 1
   for (i in 1:num_windows){
     #i = 1 #test
     ridge = glmnet(x_var[i:(window_size+i-1),], y_var[i:(window_size+i-1)], alpha = 0)
@@ -127,7 +153,7 @@ RollingWindow <- function(key_stock = 'SPY', window_size = 1000, week = 5, month
     #initializing the BIC vector
     BIC = rep(NA, length(ridge$lambda))
    
-    print(window_size+i-1)
+    
     #calculating information criteria
     for (j in 1:length(ridge$lambda)) { 
       
